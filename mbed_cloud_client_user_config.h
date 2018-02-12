919bc54a33df12591398d4602a583a61d97c53f7
Marcelo Salazar <marsal01@e109747-mac.cambridge.arm.com>
2018-02-12 19:35:19 +0000

    Remove references to FreeRTOS

--- mbed_cloud_client_user_config.h
+++ mbed_cloud_client_user_config.h
@@ -24,17 +24,12 @@
 #define MBED_CLOUD_CLIENT_TRANSPORT_MODE_TCP
 #define MBED_CLOUD_CLIENT_LIFETIME              3600
 
-#ifdef FREERTOS
-    #define SN_COAP_MAX_BLOCKWISE_PAYLOAD_SIZE       512
-#else
-    #define MBED_CLOUD_CLIENT_SUPPORT_UPDATE
-    #define SN_COAP_MAX_BLOCKWISE_PAYLOAD_SIZE       1024
-#endif
+#define MBED_CLOUD_CLIENT_SUPPORT_UPDATE
+#define SN_COAP_MAX_BLOCKWISE_PAYLOAD_SIZE       1024
 
 /* set flag to enable update support in mbed Cloud client */
-#ifndef FREERTOS
-    #define MBED_CLOUD_CLIENT_SUPPORT_UPDATE
-#endif
+#define MBED_CLOUD_CLIENT_SUPPORT_UPDATE
+
 /* set download buffer size in bytes (min. 1024 bytes) */
 
 /* Use larger buffers in Linux */
