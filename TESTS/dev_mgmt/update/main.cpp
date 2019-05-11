d807a5a97fdeb8bf61fcc924e7f4161a6edb9181
Marcelo Salazar <marcelo.salazar@arm.com>
2019-05-11 19:08:21 +0100

    Increase timeout to 120 seconds
    Fixes https://github.com/ARMmbed/simple-mbed-cloud-client/pull/64

--- TESTS/dev_mgmt/update/main.cpp
+++ TESTS/dev_mgmt/update/main.cpp
@@ -267,7 +267,7 @@ void spdmc_testsuite_update(void) {
     client.on_registered(&registered);
     client.register_and_connect();
 
-    i = 650; // wait 65 seconds
+    i = 1200; // wait 120 seconds
     while (i-- > 0 && !client.is_client_registered()) {
         wait_ms(100);
     }
